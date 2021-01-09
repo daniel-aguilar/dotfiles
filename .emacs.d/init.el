194d681f109d4953a095561cdad00ce21d0944cc
Daniel Aguilar <20218476+daniel-aguilar@users.noreply.github.com>
2021-01-08 21:58:00 -0600

    Set separate Emacs customization file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,5 +2,8 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
 (global-display-line-numbers-mode 1)
 (show-paren-mode 1)
