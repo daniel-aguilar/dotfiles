9caef2233eb72eed55ce07d23677b8f6c3c95276
Daniel Aguilar <20218476+daniel-aguilar@users.noreply.github.com>
2021-01-12 20:42:32 -0600

    Add Emacs custom settings & packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,3 +7,17 @@
 
 (global-display-line-numbers-mode 1)
 (show-paren-mode 1)
+(line-number-mode 1)
+(column-number-mode 1)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(set-default 'truncate-lines t)
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(set-frame-font "Inconsolata 12" nil t)
+
+(setq package-selected-packages
+      '(neotree zenburn-theme yaml-mode dockerfile-mode minimap))
+(load-theme 'zenburn t)
+(global-set-key [f8] 'neotree-toggle)
+(setq minimap-window-location 'right)
